aaf67906978bcd45bc3636c1e742c37dd7057368
Bruce Ricard <bruce@vps81546.vps.ovh.ca>
2016-05-20 23:39:40 -0400

    fix links

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-current_directory=`pwd`
+current_directory=$(dirname `readlink -e $0`)
 
 ln -s -i $current_directory/emacs ~/.emacs
 ln -s -i $current_directory/zshrc ~/.zshrc
 ln -s -i $current_directory/screenrc ~/.screenrc
+ln -s -i $current_directory/git-prompt.sh ~/.git-prompt.sh
