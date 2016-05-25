3a8ba5ed65cff66db8763ad8a8cf200dce13569f
Bruce Ricard <bruce.ricard@gmail.com>
2016-05-25 01:52:14 -0400

    move git clone command to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 current_directory=$(dirname `readlink -e $0`)
 
+git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zshd/zsh-git-prompt
+
 ln -s -i $current_directory/emacs ~/.emacs
 ln -s -i $current_directory/zshrc ~/.zshrc
 ln -s -i $current_directory/screenrc ~/.screenrc
